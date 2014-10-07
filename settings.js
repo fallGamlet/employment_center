ef00e663be888c1b40f14e0a2f02f8fe879364ea
Aleksandr Hilinichenko <Sasha-hi@mail.ru>
2014-10-07 14:20:46 +0300

    Переформатировал файл настоек

--- settings.js
+++ settings.js
@@ -1,18 +1,25 @@
 (function(){
-    var dbConnOptions = { driver:'mysql', host: 'localhost', port: 3311, user: 'root', password: 'root', dbname:'czn' };
+    var dbConnOptions = { 
+        driver:'mysql', 
+        host: 'localhost', 
+        port: 3311, 
+        user: 'root', 
+        password: 'root', 
+        dbname:'czn' 
+    };
     var getConnectionStr = function() {
         return util.format("%s://%s:%s@%s:%s/%s", 
-                            dbConnOptions.driver,
-                            dbConnOptions.user,
-                            dbConnOptions.password,
-                            dbConnOptions.host,
-                            dbConnOptions.port,
-                            dbConnOptions.dbname);
+                dbConnOptions.driver,
+                dbConnOptions.user,
+                dbConnOptions.password,
+                dbConnOptions.host,
+                dbConnOptions.port,
+                dbConnOptions.dbname);
     };
-	module.exports = {
-		dbfBasePath: "R:/CZNF/",
+    module.exports = {
+        dbfBasePath: "R:/CZNF/",
         dbConnOptions: dbConnOptions,
-		dbVersion: '0.0.3',
+        dbVersion: '0.0.4',
         getConnectionStr : getConnectionStr
-	}
+    };
 }).call(this);
